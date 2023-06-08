5e5afa04de127a6158d12d9aef57d164f20d3617
molokochris <molokochrisp742@gmail.com>
2023-06-08 10:27:40 +0200

    upat

--- src/pages/AddEmployee.js
+++ src/pages/AddEmployee.js
@@ -29,6 +29,63 @@ export default function AddEmployee() {
             console.log(imageSrc)
         };
     
+        //Input field
+        let employeeData = [];
+        const [firstName, setFirstName] = useState('');
+        const [surname, setSurname] = useState('');
+        const [email, setEmail] = useState('');
+        const [contactNumber, setContactNumber] = useState('');
+        const [idNumber, setIdNumber] = useState('');
+        const [employeePosition, setEmployeePosition] = useState('');
+        const [submitClicked, setSubmitClick] = useState(false);
+
+        const handleFirstNameChange = (e) => {
+            setFirstName(e.target.value);
+        };
+
+        const handleSurnameChange = (e) => {
+            setSurname(e.target.value);
+        };
+
+        const handleEmailChange = (e) => {
+            setEmail(e.target.value);
+        };
+
+        const handleContactNumberChange = (e) => {
+            setContactNumber(e.target.value);
+        };
+
+        const handleIdNumberChange = (e) => {
+            setIdNumber(e.target.value);
+        };
+
+        const handleEmployeePositionChange = (e) => {
+            setEmployeePosition(e.target.value);
+        };
+        const handleSubmit = (e) => {
+            e.preventDefault();
+
+            employeeData.push({
+                "firstName" : firstName,
+                "Surname": surname,
+                "contacts": contactNumber,
+                "email": email,
+                "idNumber": idNumber,
+                "position": employeePosition,
+                "imageSrc": imageSrc
+            })
+            console.log(employeeData);
+
+            setFirstName('');
+            setSurname('');
+            setContactNumber('');
+            setEmail('');
+            setIdNumber('');
+            setEmployeePosition('');
+            setImageSrc("./Prof-placeholder.png")
+
+            setSubmitClick(!submitClicked);
+        }
 
     return (
         <div className="add-employee">
@@ -43,31 +100,54 @@ export default function AddEmployee() {
             </div>
             
             <div className="input-form">
-                <input
-                    type="text"
-                    placeholder="First Names"
-                />
-                <input
-                    type="text"
-                    placeholder="Surname"
-                />
-                <input
-                    type="email"
-                    placeholder="Email"
-                />
-                <input type="number"
-                    placeholder="Contact Number"
-                />
-                <input
-                    type="number"
-                    placeholder="ID Number"
-                />
-                <input
-                    type="text"
-                    placeholder="Employee Position"
-                />
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="First Names"
+                        value={firstName}
+                        onChange={handleFirstNameChange} 
+                    />
+                    <input
+                        type="text"
+                        placeholder="Surname"
+                        value={surname}
+                        onChange={handleSurnameChange} 
+                    />
+                    <input
+                        type="number"
+                        placeholder="Contact numbers"
+                        value={contactNumber}
+                        onChange={handleContactNumberChange} 
+                    />
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={handleEmailChange} 
+                    />
+                    <input
+                        type="number"
+                        placeholder="ID Number"
+                        value={idNumber}
+                        onChange={handleIdNumberChange} 
+                    />
+                    <input
+                        type="text"
+                        placeholder="Employee Position"
+                        value={employeePosition}
+                        onChange={handleEmployeePositionChange} 
+                    />
+                    <br />
+                    <button type="submit">add</button>
+                    { submitClicked && (<>
+                    <br />
+                    <button onClick={console.log("view then employees")}>
+                            view employees
+                    </button>
+                    </>)}
+                    
+                </form>
             </div>
-            <button>Add</button>
         </div>
     );
 }
